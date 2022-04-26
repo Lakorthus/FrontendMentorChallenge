33547f7dffc4c47430f06df7a1772ba38f314d5e
Lakorthus <julio28ve@gmail.com>
2022-04-26 01:02:18 +0100

    script and styles

--- base-apparel-coming-soon-master/script.js
+++ base-apparel-coming-soon-master/script.js
@@ -1,1 +1,22 @@
-// javascripts
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	const emailVal = email.value;
+
+	// check if it is a valid email
+	if (!validateEmail(emailVal)) {
+		form.classList.add('error');
+	} else {
+		form.classList.remove('error');
+		document.body.innerHTML = `<div style="display:flex; align-items: center; justify-content: center;"><h1>We will be in touch!</h1></div>`;
+	}
+});
+
+// funtion from stackOverFlow
+
+function validateEmail(email) {
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+}
