fc685450b88e2cb9d59dd8aae5d2401cdd5072c3
Lakorthus <julio28ve@gmail.com>
2022-05-14 22:56:54 +0100

    script and new challenge

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
 const projects = [
-    'four-card-section-master',
-    'single-price-grid-component-master',
-    'base-apparel-coming-soon-master',
-    'intro-component-with-signup-form-master',
-    'huddle-landing-page-with-single-introductory-section-master',
-    'huddle-landing-page-master',
-    'fylo-landing-page-with-two-column-layout-master',
-    'qr-code-component-main',
-    'nft-preview-card-component-main',
-    'order-summary-component-main',
-    'stats-preview-card-component-main',
-    '3-column-preview-card-component-main',
-    'social-proof-section-master',
-    'profile-card-component-main'
+  "single-price-grid-component-master",
+  "nft-preview-card-component-main",
+  "order-summary-component-main",
+  "qr-code-component-main",
+  "interactive-rating-component-main",
+  "3-column-preview-card-component-main",
+  "profile-card-component-main",
+  "intro-component-with-signup-form-master",
+  "base-apparel-coming-soon-master",
+  "fylo-landing-page-with-two-column-layout-master",
+  "stats-preview-card-component-main",
+  "four-card-section-master",
+  "social-proof-section-master",
+  "huddle-landing-page-with-single-introductory-section-master",
+  "huddle-landing-page-master",
 ];
 
-const list = document.getElementById('list');
+const list = document.getElementById("list");
 
-projects.forEach((project,i) => {
-    const listItem = document.createElement('li');
-    const link = document.createElement('a');
-    link.href = `/${project}/index.html`;
-    link.innerText = `${i+1}.${formatProjectName(project)}`;
+projects.forEach((project, i) => {
+  const listItem = document.createElement("li");
+  const link = document.createElement("a");
+  link.href = `/${project}/index.html`;
+  link.innerText = `${i + 1}.${formatProjectName(project)}`;
 
-    const img = document.createElement('img');
-    img.src = `/${project}/design/desktop-design.jpg`;
+  const img = document.createElement("img");
+  img.src = `/${project}/design/desktop-design.jpg`;
 
-    link.prepend(img);
-    listItem.appendChild(link);
-    list.appendChild(listItem);
+  link.prepend(img);
+  listItem.appendChild(link);
+  list.appendChild(listItem);
 });
 
 function formatProjectName(name) {
-    return name
-    .split('-').map(word => word [0].toUpperCase() + word.slice(1)).join(' ');
+  return name
+    .split("-")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
 }
